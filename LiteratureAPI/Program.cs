25e71ab807fc3f5ea17a20627f9938dc05d769d7
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-12-18 11:02:32 +0000

    feat: Setup WebApplication with MVC pattern outline

--- LiteratureAPI/Program.cs
+++ LiteratureAPI/Program.cs
@@ -1,6 +1,33 @@
 var builder = WebApplication.CreateBuilder(args);
+
+/*
+ Registers everything needed for Web API Development, see 
+https://stackoverflow.com/questions/75641248/what-exactly-does-builder-services-addcontrollers-do
+https://learn.microsoft.com/en-us/aspnet/core/fundamentals/routing?view=aspnetcore-9.0
+ */
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+/*
+ Setup route matching
+ https://stackoverflow.com/questions/76949213/how-does-the-userouting-method-work-in-asp-net-core
+ https://www.tektutorialshub.com/asp-net-core/asp-net-core-middleware-request-pipeline/
+
+"UseRouting adds route matching to the middleware pipeline. 
+This middleware looks at the set of endpoints defined in the app, 
+and selects the best match based on the request."
+ */
+app.UseRouting();
+
+/*
+ Setup an EndpointMiddleware, mapping our controllers to it...?
+ "UseEndpoints adds endpoint execution to the middleware pipeline. 
+It runs the delegate associated with the selected endpoint."
+ */
+app.UseEndpoints(endpoints =>
+{
+    _ = endpoints.MapControllers();
+});
 
 app.Run();
